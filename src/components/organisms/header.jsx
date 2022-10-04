ee428be9bbc153f06c6f16db301e9e276132dc4a
Chelim Lee <leecr1215@naver.com>
2022-10-04 21:18:50 +0900

    chore : js to jsx

---
+++ src/components/organisms/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+const Head = styled.div`
+  width: 100%;
+  height: 3em;
+  background-color: ${theme.primaryColor};
+`;
+
+const Header = () => {
+  const navigate = useNavigate();
+  return <Head />;
+};
+
+export default Header;
---
+++ src/styles/theme.jsx
@@ -0,0 +1,6 @@
+const theme = {
+  primaryColor: "#76a5afff",
+  secondaryColor: "#a2c4c9ff",
+};
+
+export default theme;
