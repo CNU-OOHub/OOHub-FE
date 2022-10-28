cddba1189a50a96657fba7fd2bbce91eda0e47a4
Songyi Kim <ksl2950@naver.com>
2022-10-28 22:12:02 +0900

    fix: 모니터링 css 수정

--- src/components/pages/monitoring.jsx
+++ src/components/pages/monitoring.jsx
@@ -5,14 +5,20 @@ import theme from "../../styles/theme";
 import { useGetResources , useWorkspaceUsage } from "../../api";
 import { useEffect, useState } from "react";
 
+const Frame = styled.div`
+  width: 960px;
+  height: 50px;
+  background-color: white;
+  margin-left: 50px;
+  margin-right: 30px;
+  border: 1px solid black;
+`;
 
 const Mdiv = styled.div`
-  width: ${(props) => `${props.width}%`};
-  height: 30px;
+  width: ${(props) => `${Number(props.width)}%`};
+  height: 48px;
   background-color: ${theme.primaryColor};
-  margin-left: 50px;
-  margin-right: 20px;
-  border: 1.5px solid black;
+  float: left;
 `;
 
 const Monitoring = () => {
@@ -26,24 +32,25 @@ const Monitoring = () => {
   
   return (
     (typeof resources !== "undefined")&& (typeof workspaceUsage !== "undefined") ? (<div style={{ height: "92vh" }}>
-    <div style={{ paddingTop: 50, paddingLeft:50, paddingBottom:10}}>
-    <Text fontSize='1.7' fontWeight='bold'> {'>'} Resource Monitoring For admin</Text>
+    <div style={{ paddingTop: 50, paddingLeft:50}}>
+    <Text fontSize='2' fontWeight='bold'> {'>'} Resource Monitoring For admin</Text>
     </div>
     <div style={{marginLeft:"5vh", width: '100%'}}>
-      <div style={{ margin: 70, paddingLeft: 60, display: 'flex'}}>
-      <Text fontSize='1.5'> CPU </Text>
-      <Mdiv width={resources.data.cpuUsagePercent}></Mdiv>
-      <Text fontSize='1.5'> {resources.data.cpuUsagePercent}% </Text>
+      <div style={{ margin: 70, paddingLeft: 60, display: 'flex', justifyItems: 'centers'}}>
+      <Text fontSize='1.7'> CPU </Text>
+      <Frame><Mdiv width={resources.data.cpuUsagePercent}></Mdiv></Frame>
+      <Text fontSize='1.7'> {resources.data.cpuUsagePercent}% </Text>
       </div >
+
       <div style={{ margin: 70, paddingLeft: 60, display: 'flex' }}>
-      <Text fontSize='1.5'> GPU </Text>
-      <Mdiv width='0'></Mdiv>
-      <Text fontSize='1.5'> 0.00% </Text>
+      <Text fontSize='1.7'> GPU </Text>
+      <Frame><Mdiv width='0'></Mdiv></Frame>
+      <Text fontSize='1.7'> 0.00% </Text>
       </div>
       <div style={{ margin: 70, paddingLeft: 60, display: 'flex' }}>
-      <Text fontSize='1.5'> RAM </Text>
-      <Mdiv width={resources.data.usedRamUsage}> </Mdiv>
-      <Text fontSize='1.5'> {resources.data.usedRamUsage } Mib </Text>
+      <Text fontSize='1.7'> RAM </Text>
+      <Frame><Mdiv width={resources.data.usedRamUsage}> </Mdiv></Frame>
+      <Text fontSize='1.7'> {resources.data.usedRamUsage } Mib </Text>
       </div>
       <div style={{ textAlign: 'center',justifyContent: 'space-between', paddingTop: 20 }}>
       <Text fontSize='2' fontWeight="40"> 사용한 공간 </Text>
