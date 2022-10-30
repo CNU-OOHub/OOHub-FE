c541a01fd5e7afcf611710e8a95fa17bfa7ec37d
Chelim Lee <leecr1215@naver.com>
2022-10-30 18:00:47 +0900

    refactor: sideMenu 컴포넌트로

---
+++ src/components/molecules/sideMenu.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import theme from "../../styles/theme";
+import styled from "styled-components";
+import { useState } from "react";
+import { FOLDER, GROUPS, SETTING } from "../../constants";
+import Button from "../atoms/button";
+import { AiOutlineFolderOpen, AiOutlineSetting } from "react-icons/ai";
+import { RiGroup2Line } from "react-icons/ri";
+
+const SideMenuStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 5rem;
+  height: 92vh;
+  background-color: ${theme.greyColor};
+`;
+
+const SideMenu = () => {
+  const [sideMenu, setSideMenu] = useState(FOLDER);
+  const sideMenuClicked = (clickedValue) => {
+    setSideMenu(clickedValue);
+  };
+  return (
+    <SideMenuStyle>
+      <Button bgColor={theme.greyColor} onClick={() => sideMenuClicked(FOLDER)}>
+        <AiOutlineFolderOpen
+          style={{
+            marginTop: "1rem",
+            width: "100%",
+            borderLeft: sideMenu === FOLDER ? "5px solid white" : "none",
+          }}
+          size={35}
+          color={sideMenu === FOLDER ? "white" : theme.lightGreyColor}
+        />
+      </Button>
+      <Button
+        bgColor={theme.greyColor}
+        onClick={() => sideMenuClicked(SETTING)}
+      >
+        <AiOutlineSetting
+          style={{
+            marginTop: "1rem",
+            width: "100%",
+            borderLeft: sideMenu === SETTING ? "5px solid white" : "none",
+          }}
+          size={35}
+          color={sideMenu === SETTING ? "white" : theme.lightGreyColor}
+        />
+      </Button>
+      <Button bgColor={theme.greyColor} onClick={() => sideMenuClicked(GROUPS)}>
+        <RiGroup2Line
+          style={{
+            marginTop: "1rem",
+            width: "100%",
+            borderLeft: sideMenu === GROUPS ? "5px solid white" : "none",
+          }}
+          size={35}
+          color={sideMenu === GROUPS ? "white" : theme.lightGreyColor}
+        />
+      </Button>
+    </SideMenuStyle>
+  );
+};
+
+export default SideMenu;
