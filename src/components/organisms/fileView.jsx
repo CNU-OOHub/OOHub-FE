eeba6b2604fbed22fed95be9c231b7e68acc12ab
Songyi Kim <52441906+songyi00@users.noreply.github.com>
2022-11-01 22:11:49 +0900

    feat: 콘솔 실행 및 데이터 바인딩 (#38)
    * feat: 파일 실행 데이터 바인딩
    
    * fix: 충돌 해결
    
    * feat: 실행버튼 마우스 오버 추가
    
    * feat: 콘솔, 터미널 css 구분
    
    * fix: 터미널 css 수정
    
    * fix: 파일 테두리 css 수정
    
    * feat: 터미널 데이터 바인딩
    
    * feat: 터미널 명령어 실행
    
    * fix: 충돌 해결

--- src/components/organisms/fileView.jsx
+++ src/components/organisms/fileView.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState,useRecoilValue } from "recoil";
 import { adminState, fileShareState, loginState } from "../../atom";
 import theme from "../../styles/theme";
+import Body from "../atoms/body";
 import {
+  AiOutlineFolderOpen,
+  AiOutlineSetting,
   AiOutlineSearch,
   AiFillCaretDown,
   AiFillCaretUp,
 } from "react-icons/ai";
-import { IoCloseOutline } from "react-icons/io5";
-import { BsFolderPlus } from "react-icons/bs";
-import { FiFilePlus } from "react-icons/fi";
+import { IoIosClose } from "react-icons/io";
+import { RiGroup2Line } from "react-icons/ri";
 import Button from "../atoms/button";
-import { CONSOLE, TERMINAL } from "../../constants";
+import { CONSOLE, FOLDER, GROUPS, SETTING, TERMINAL } from "../../constants";
 import styled from "styled-components";
 import FlexRow from "../molecules/flexRow";
 import Text from "../atoms/text";
@@ -19,10 +21,11 @@ import DropDown from "../atoms/dropdown";
 import Switch from "react-switch";
 import Input from "../atoms/input";
 import { VscRunAll } from "react-icons/vsc";
-import { Fragment } from "react";
-import FlexColumn from "../molecules/flexColumn";
-import { useRef } from "react";
-import { useGetAllOrganizations } from "../../api";
+import { runFile, runLine, useGetAllOrganizations } from "../../api";
+import { QueryClient, useMutation } from "@tanstack/react-query";
+import { IoCloseOutline } from "react-icons/io5";
+import { BsFolderPlus } from "react-icons/bs";
+import { FiFilePlus } from "react-icons/fi";
 
 const FileList = styled.div`
   display: flex;
@@ -83,13 +86,51 @@ const TerminalHeader = styled.div`
   margin-left: 1rem;
 `;
 
+const Scroll = styled.div`
+  width: 100%;
+  height: 100%;
+  align-content: center;
+  align-items: center;
+  text-align: center;
+  overflow: scroll;
+  // margin-top: 1.5rem;
+  padding: 0.5rem;
+  border-radius: 10px;
+  background: ${theme.blackGreyColor};
+`;
+
 const FileView = () => {
   const [login, setLogin] = useRecoilState(loginState);
+  const [admin, setAdmin] = useRecoilState(adminState);
   const [sharedFileMenuOpened, SetSharedFileMenuOpened] = useState(false);
   const [myFileMenuOpened, SetMyFileMenuOpened] = useState(false);
   const [fileShare, setFileShare] = useRecoilState(fileShareState);
   const [openedFile, setOpenedFile] = useState("파일명");
   const [terminalOpened, setTerminalOpened] = useState(CONSOLE);
+
+  const [fileContents, setFileContents] = useState({
+    contents: ""
+  });
+  // 터미널 input 입력값 
+  const [terminalLine, setTerminalLine] = useState({
+    command: ""
+  });
+  // 터미널 input 명령어, 결과값 map list 
+  // command: "",
+  // runTerminalResult: []
+  const [terminalDivList, setTerminalDivList] = useState([]);
+  const [runConsoleResult, setRunConsoleResult] = useState([]);
+  const [runTerminalResult, setRunTerminalResult] = useState([]);
+
+  
+  useEffect(() => {
+    if (sessionStorage.getItem("accessToken")) {
+      setLogin(true);
+    }
+    if (localStorage.getItem("isAdmin") === "true") {
+      setAdmin(true);
+    }
+  });
 
   useEffect(() => {
     if (sessionStorage.getItem("accessToken")) {
@@ -100,6 +141,44 @@ const FileView = () => {
   const terminalClicked = (clickedValue) => {
     setTerminalOpened(clickedValue);
   };
+
+  const changeFileContent = (name, changedValue) => {
+    setFileContents((prev) => ({...prev, [name]: changedValue}));
+    // console.log(fileContents);
+  };
+
+  const changeTerminalLine = (name, changedValue) => {
+    setTerminalLine((prev) => ({...prev, [name]: changedValue}));
+    console.log(terminalLine);
+  };
+
+  const executeFileMutation = useMutation((fileContents) => runFile(fileContents), {
+    onSuccess: (data) => {
+      setRunConsoleResult(data.result);
+    },
+  });
+
+  const executeTerminalMutation = useMutation((terminalLine) => runLine(terminalLine), {
+    onSuccess: (data) => {
+      console.log(data.result[0]);
+      setRunTerminalResult(data.result);
+      setTerminalDivList(...terminalDivList,data.result[0]);
+    },
+  });
+
+  // 파일 실행 api 호출 
+  const executeFile = () => {
+    executeFileMutation.mutate(fileContents);
+  };
+
+  // 터미널 실행 
+  const terminalEntered = e => {
+    if(e.key === 'Enter'){
+      executeTerminalMutation.mutate(terminalLine);
+    }
+  }
+
+
   const { data: groups, isLoading } = useGetAllOrganizations(
     localStorage.getItem("username")
   );
@@ -255,101 +334,136 @@ const FileView = () => {
         )}
       </FileList>
       <File>
-        <FileHeader>
-          <FlexRow justifyContent="center" flexGrow={1}>
-            <Text color={theme.textGreyColor} fontSize={1}>
-              {openedFile}
-            </Text>
-            <IoCloseOutline size={20} color={theme.textGreyColor} />
-            <VscRunAll size={20} color="green" />
-          </FlexRow>
-          <div style={{ width: "6rem" }}>
-            <DropDown
-              onChange={(e) => {
-                setFileShare((prev) => ({
-                  ...prev,
-                  groupName: e.target.value,
-                }));
-              }}
-              options={
-                isLoading ? [] : Array.from(groups, (group) => group.name)
-              }
-              placeholder="그룹명"
-              color={theme.textGreyColor}
-              height={2}
-              fontSize={1.0}
-              backgroundColor="#373737"
-            />
-          </div>
-          <FlexRow flexGrow={1} justifyContent="center">
-            <Text color={theme.textGreyColor} fontSize={1}>
-              공유
-            </Text>
-            <Switch
-              onChange={(e) => {
-                if (fileShare.groupName === "") {
-                  alert("그룹명을 선택하여 주세요.");
-                } else {
+          <FileHeader>
+            <FlexRow justifyContent="center" flexGrow={1}>
+              <Text color={theme.textGreyColor} fontSize={1}>
+                {openedFile}
+              </Text>
+              <IoIosClose size={20} color={theme.textGreyColor} />
+              <div style={{marginLeft:"3vh"}}>
+              <VscRunAll IoIosClose size="20" color="green" style={{cursor:"pointer"}} onClick={() => {
+                  executeFile();
+                }}/>
+              </div>
+            </FlexRow>
+            <div style={{ width: "6rem" }}>
+              <DropDown
+                options={[]}
+                placeholder="그룹명"
+                color={theme.textGreyColor}
+                height={2}
+                fontSize={1.0}
+                backgroundColor="#373737"
+              />
+            </div>
+            <FlexRow flexGrow={1} justifyContent="center">
+              <Text color={theme.textGreyColor} fontSize={1}>
+                공유
+              </Text>
+              <Switch
+                onChange={(e) => {
                   setFileShare((prev) => ({
                     ...prev,
                     available: e,
                   }));
+                }}
+                checked={fileShare.available}
+                onColor={theme.primaryColor}
+                handleDiameter={17}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                width={45}
+                height={25}
+              />
+            </FlexRow>
+          </FileHeader>
+          <FileContainer>
+          <FileContent 
+            name="fileContentArea" 
+            onChange={(e) => {
+                changeFileContent("contents",e.target.value);
+              }} 
+            onKeyDown = {(e) => {
+              // enterEvent()
+            }}>
+          </FileContent>
+
+          </FileContainer>
+          <Terminal>
+            <TerminalHeader>
+              <Button
+                color={
+                  terminalOpened === CONSOLE ? "white" : theme.lightGreyColor
                 }
-              }}
-              checked={fileShare.available}
-              onColor={theme.primaryColor}
-              handleDiameter={17}
-              uncheckedIcon={false}
-              checkedIcon={false}
-              width={45}
-              height={25}
-            />
-          </FlexRow>
-        </FileHeader>
-        <FileContainer>
-          <FileContent name="inputstr2"></FileContent>
-        </FileContainer>
-        <Terminal>
-          <TerminalHeader>
-            <Button
-              color={
-                terminalOpened === CONSOLE ? "white" : theme.lightGreyColor
-              }
-              fontSize={0.8}
-              bgColor={theme.blackGreyColor}
-              height={2}
-              marginRight={2}
-              fontWeight={400}
-              onClick={() => {
-                terminalClicked(CONSOLE);
-                console.log(fileShare.available);
-              }}
-              {...(terminalOpened === CONSOLE && {
-                borderBottom: "1px solid white",
+                fontSize={0.8}
+                bgColor={theme.blackGreyColor}
+                height={2}
+                marginRight={2}
+                fontWeight={400}
+                onClick={() => {
+                  terminalClicked(CONSOLE);
+                  console.log(fileShare.available);
+                }}
+                {...(terminalOpened === CONSOLE && {
+                  borderBottom: "1px solid white",
+                })}
+              >
+                콘솔
+              </Button>
+              <Button
+                color={
+                  terminalOpened === TERMINAL ? "white" : theme.lightGreyColor
+                }
+                fontSize={0.8}
+                bgColor={theme.blackGreyColor}
+                height={2}
+                fontWeight={400}
+                onClick={(e) => {
+                  terminalClicked(TERMINAL);
+                }}
+                {...(terminalOpened === TERMINAL && {
+                  borderBottom: "1px solid white",
+                })}
+              >
+                터미널
+              </Button>
+            </TerminalHeader>
+            {terminalOpened===TERMINAL? 
+            // 터미널 
+              <Scroll>
+              <br></br>
+              <div style={{color:"white", padding:"10px",float:"left"}}>
+              {
+                  terminalDivList.map((result,i)=>{
+                    console.log(result[i]);
+                    return <p color="white" style={{fontSize: "1.2rem", fontWeight:"normal",}}>{result[i]}</p>;
+                  })              
+                } 
+              <text style={{color:"white", float:"left" ,outline: "none", fontWeight:"bolder"}}>{'>>>  '} </text>             
+                <input 
+                type={"text"} 
+                style={{outline:"none",backgroundColor:theme.blackGreyColor, color:"white", border:"none", float:"left", marginLeft:"10px"}}
+                onChange={(e) => {
+                  changeTerminalLine("command",e.target.value);
+                }} 
+                onKeyDown = {(e) => {
+                terminalEntered(e)
+                }}
+                ></input>
+              </div>;
+              </Scroll>
+            :
+            // 콘솔 
+            <Scroll>
+            <div style={{color:"white", padding:"10px",float:"left"}}>
+              {runConsoleResult.map((result)=>{
+                return <p color="white" style={{fontSize: "1.2rem", fontWeight:"normal",}}>{result}</p>;
               })}
-            >
-              콘솔
-            </Button>
-            <Button
-              color={
-                terminalOpened === TERMINAL ? "white" : theme.lightGreyColor
-              }
-              fontSize={0.8}
-              bgColor={theme.blackGreyColor}
-              height={2}
-              fontWeight={400}
-              onClick={(e) => {
-                terminalClicked(TERMINAL);
-              }}
-              {...(terminalOpened === TERMINAL && {
-                borderBottom: "1px solid white",
-              })}
-            >
-              터미널
-            </Button>
-          </TerminalHeader>
-        </Terminal>
-      </File>
+            </div>
+            </Scroll>
+            }
+          </Terminal>
+        </File>
     </>
   );
 };
